3289280fc24e3ca2efa5955956cf15240ee1bf90
KapiPa <163605860+KapiPa@users.noreply.github.com>
2024-11-05 06:46:48 -0300

    Update reportes.php

--- sistema_restaurante/vistas/reportes/reportes.php
+++ sistema_restaurante/vistas/reportes/reportes.php
@@ -7,6 +7,7 @@
 <body>
     <h1>Panel de Reportes</h1>
     <ul>
+        <li><a href="../../menu.php">Menu</a></li>
         <li><a href="reporte_ventas.php">Informe de Ventas</a></li>
         <li><a href="reporte_pedidos.php">Informe de Pedidos</a></li>
         <li><a href="reporte_inventario.php">Informe de Inventario</a></li>
